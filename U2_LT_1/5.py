689e499960d9b3db43bededd0a14d98914c838b3
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2025-08-02 09:22:23 +0530

    Add files via upload

---
+++ U2_LT_1/5.py
@@ -0,0 +1,32 @@
+#  5. Store attendance (out of 30) for 12 students.
+# Options:
+# a) Display students who attended less than 20 days.
+# b) Replace attendance below 15 with 15.
+# c) Find the average attendance
+
+import numpy as np
+
+attendance = np.random.randint(0, 31, size=12)  
+print("Attendance for 12 students (out of 30):")
+print(attendance)
+print("=====================================================")
+
+
+print("a) Display students who attended less than 20 days.")
+count=0
+for i in attendance:
+    if i < 20:
+        print("attended less than 20 days : ",i)
+        count+=1
+
+print("=====================================================")
+print("b) Replace attendance below 15 with 15.")
+
+for i in range(len(attendance)) :
+    if  attendance[i] <=15:
+        attendance[i]= 15
+print(attendance)
+print("=====================================================")
+print("c) Find the average attendance")
+
+print(f"Avarage  is : {sum(attendance)/len(attendance)}")
---
+++ U2_LT_1/6.py
@@ -0,0 +1,35 @@
+# 6. Create a 1D array showing the number of beds occupied in 7 hospital wards.
+# Options:
+# a) Find the ward with maximum occupancy.
+# b) Replace any value below 5 with 5.
+# c) Count wards with occupancy above 20
+
+import numpy as np
+
+hospital = np.random.randint(0, 25, size=7)  
+print("hospital for 12 students (out of 30):")
+print(hospital)
+print("=====================================================")
+
+
+print("a) Find the ward with maximum occupancy.")
+print(f"Highestsalary is : {max(hospital)}")
+
+
+
+print("=====================================================")
+print("b) Replace hospital below 5 with 5.")
+
+for i in range(len(hospital)) :
+    if  hospital[i] <=5:
+        hospital[i]= 5
+print(hospital)
+
+print("=====================================================")
+print("c) Count wards with occupancy above 20")
+
+count=0
+for i in hospital:
+    if i >= 20:
+        print("wards : ",i)
+        count+=1
