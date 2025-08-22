75b48504155fd7d7e5f12a4eb0d6283a2f5dfb35
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2025-08-22 20:51:59 +0530

    Delete LAB_TASK_2 directory

--- LAB_TASK_2/13.py
+++
@@ -1,9 +0,0 @@
-# 13. Combine Two Lists into a Patient Dictionary
-keys = ["Name", "Age", "Disease"]
-values = ["Meena", 42, "Diabetes"]
-
-# Combine using zip and dict()
-patient = dict(zip(keys, values))
-
-print("Patient Record:")
-print(patient)
--- LAB_TASK_2/9.py
+++
@@ -1,3 +0,0 @@
-# 9. Dictionary Comprehension for Cube of Numbers 1 to 5
-cubes = {x: x**3 for x in range(1, 6)}
-print("Cubes Dictionary:", cubes)
