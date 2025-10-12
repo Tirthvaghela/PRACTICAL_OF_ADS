62f1282c094e82330a521cc44be52b558b504097
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2025-10-12 13:37:08 +0530

    Delete U1_LT_1 directory

--- U1_LT_1/LAB_TASK_1/Q2_vol.py
+++
@@ -1,7 +0,0 @@
-def calculate_sphere_volume(radius):
-  volume = (4/3) * 3.14 * radius * radius * radius
-  return volume
-
-def calculate_cube_volume(side_length):
-  volume = side_length * side_length * side_length
-  return volume
