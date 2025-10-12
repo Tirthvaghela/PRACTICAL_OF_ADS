ed1e9acfe67511030b2b3c6b1ff89f392754c613
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2025-10-12 13:38:16 +0530

    Delete U3_LT_2 directory

--- U3_LT_2/14.py
+++
@@ -1,1 +0,0 @@
-# 14. Excel sales.xlsx with Date,Sales. Filter sales between '2024-01-01' and '2024-02-01'.
--- U3_LT_2/3.py
+++
@@ -1,8 +0,0 @@
-# 3. Assume 'sales.csv' with columns Region,Sales.
-# Read CSV and display descriptive stats using describe().
-
-import pandas as pd
-df = pd.read_csv('sales.csv')
-print("=====================================================")
-print(df.describe())    
-print("=====================================================")
